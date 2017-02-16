1c48d275da7150c1cab74bc3c99bafce32a248fd
Aleksandr Neizv <Aleksandr Neizv>
2017-02-16 14:10:18 +0300

    Исходник

---
+++ HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maxNumber 
+{
+    using static Console;
+    class Program
+    {
+        // Находит наибольшее из 3-ех чисел
+        static int maxNumber(int a, int b, int c)
+        {            
+            if (a>b && a>c)
+            {
+                return a;
+            }
+            else if (b>c && b>a)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        }    
+        static void Main(string[] args)
+        {
+            WriteLine("Введите три числа");
+            int a = Convert.ToInt32(ReadLine());
+            int b = Convert.ToInt32(ReadLine());
+            int c = Convert.ToInt32(ReadLine());
+
+            WriteLine($"Наибольшее число {maxNumber(a, b, c)}");
+            ReadKey();
+        }
+    }
+}
---
+++ HomeWork2/imt/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imt
+{
+    using static Console;
+    using static Convert;
+    class Program
+    {
+        // Расчет ИМТ
+        static double imt(double m, double h)
+        {
+            double i = m / (h * h);
+            return i;
+        }
+        
+        // Определяет, соответствует ИМТ норме или нет
+        static bool checkImt(double imt1)
+        {
+            if (imt1>=18.5 && imt1<=25)
+            {
+                return true; //все в норме))
+            }
+            else
+            {
+                return false; // все плохо((
+            }
+        }
+
+        // Защита от дурака
+        static double foolproof()
+        {
+            Double x = 0;
+            bool prot = false;
+            while (!prot)
+            {
+                prot = Double.TryParse(ReadLine(), out x);
+                if (prot)
+                {
+                    break;
+                }
+                WriteLine("Значение не допустимо, повторите ввод");                
+            }
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            WriteLine("Введите свой вес");
+            double x = foolproof();
+            WriteLine("Введите свой рост в метрах");
+            double y = foolproof();            
+
+            if (checkImt(imt(x, y)))
+            {
+                WriteLine("Вес в норме!");
+            }
+            else if (imt(x, y) < 18.5)
+            {
+                int a = ToInt32(18.5 * y * y - x);
+                WriteLine($"Недостаточная масса тела. Нужно набрать {a} кг до нормы");
+            }
+            else
+            {
+                int a = ToInt32(x - 25 * y * y);
+                WriteLine($"Избыточная масса тела. Нужно похудеть на {a} кг");
+            }
+            ReadKey();
+        }
+    }
+}
